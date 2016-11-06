8f36866a9abfbfb6f5b219bb0171674f5d406a67
Ryan Waits <ryan.waits@gmail.com>
2016-11-06 13:55:05 -0600

    update article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,33 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    
+  end
+
+  def create
+    @article = Article.create(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
