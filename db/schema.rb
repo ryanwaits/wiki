0c2b005f213e7beb24fe2783709ef82e03384685
Ryan Waits <ryan.waits@gmail.com>
2016-11-06 15:02:24 -0600

    create categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106190548) do
+ActiveRecord::Schema.define(version: 20161106201723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,17 @@ ActiveRecord::Schema.define(version: 20161106190548) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "categories"
 end
