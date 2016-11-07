50318bfba7f7a201a988c45ebcf149fe3c749375
Ryan Waits <ryan.waits@gmail.com>
2016-11-06 18:50:54 -0600

    create users table and add bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106201723) do
+ActiveRecord::Schema.define(version: 20161107004633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 20161106201723) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "user_id"
     t.index ["category_id"], name: "index_articles_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
   end
 
   create_table "categories", force: :cascade do |t|
@@ -30,5 +32,13 @@ ActiveRecord::Schema.define(version: 20161106201723) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
   add_foreign_key "articles", "categories"
+  add_foreign_key "articles", "users"
 end
